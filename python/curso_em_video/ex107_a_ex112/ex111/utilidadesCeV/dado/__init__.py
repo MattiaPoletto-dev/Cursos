c714213da2b9a084fd6f96cedb794bf851123a3f
Mattia Poletto <mpoletto.dev@gmail.com>
2025-10-03 16:43:02 -0300

    arrumei

--- python/curso_em_video/ex107_a_ex112/ex111/utilidadesCeV/dado/__init__.py
+++
@@ -1,9 +0,0 @@
-def leiaDinheiro(msg):
-    valido = False
-    while not valido:
-        entrada = str(input(msg)).lower().strip().replace(',','.')
-        if entrada.isalpha() or entrada == '':
-            print(f'\033[91mERRO: \"{entrada}\" é um preço inválido!\033[m')
-        else:
-            valido = True
-            return float(entrada)
